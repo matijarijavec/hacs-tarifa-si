e68b3cd2ad654304dba4ddcd4e25a18267920e87
Matija Rijavec <matija.rijavec@gmail.com>
2024-10-04 08:05:26 +0200

    scan_interval fix

--- custom_components/tarifa_si/sensor.py
+++ custom_components/tarifa_si/sensor.py
@@ -6,21 +6,24 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-# Set the update interval (every 2 minutes)
-SCAN_INTERVAL = timedelta(minutes=2)
+# Default scan interval
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=5)
 
 URL = "https://www.tarifa.si/api/tarifa/trenutna"
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
+    # Get the scan interval from the configuration, or default to DEFAULT_SCAN_INTERVAL
+    scan_interval = config.get("scan_interval", DEFAULT_SCAN_INTERVAL)
 
-    # Get the scan interval from the configuration, default to SCAN_INTERVAL
-    scan_interval = config.get("scan_interval", SCAN_INTERVAL)
-
-    # Create the tariff sensor
+    # Create the tariff sensor with the scan interval
     data = TarifaSiData()
-    add_entities([TarifaSiSensor(data, scan_interval)], True)
+    add_entities([TarifaSiSensor(data)], True)
+
+    # Override the SCAN_INTERVAL in the config with what is provided in configuration.yaml
+    global SCAN_INTERVAL
+    SCAN_INTERVAL = timedelta(seconds=scan_interval)
 
 
 class TarifaSiData:
@@ -62,17 +65,11 @@ class TarifaSiData:
 class TarifaSiSensor(SensorEntity):
     """Representation of the tarifa_si sensor."""
 
-    def __init__(self, data, scan_interval):
+    def __init__(self, data):
         """Initialize the sensor."""
         self._data = data
         self._state = None
         self._attributes = {}
-        self._scan_interval = scan_interval  # Store the scan interval
-
-    @property
-    def scan_interval(self):
-        """Return the scan interval for this sensor."""
-        return self._scan_interval
 
     @property
     def name(self):
@@ -93,11 +90,6 @@ class TarifaSiSensor(SensorEntity):
     def extra_state_attributes(self):
         """Return the other key/values as attributes."""
         return self._attributes
-
-    @property
-    def unique_id(self):
-        """Return a unique identifier for this sensor."""
-        return "tarifa_si_sensor"
 
     def update(self):
         """Fetch new state data for the sensor."""
